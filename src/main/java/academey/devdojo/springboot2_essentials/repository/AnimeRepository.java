e17652f3207e4dfc82ab914a945f6901f98ad560
Guilherme Luan <guilhermeluan@pm.me>
2024-07-06 18:58:03 -0300

    Request Params

--- src/main/java/academey/devdojo/springboot2_essentials/repository/AnimeRepository.java
+++ src/main/java/academey/devdojo/springboot2_essentials/repository/AnimeRepository.java
@@ -3,5 +3,9 @@ package academey.devdojo.springboot2_essentials.repository;
 import academey.devdojo.springboot2_essentials.domain.Anime;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AnimeRepository extends JpaRepository<Anime, Long> {
+
+    List<Anime> findByName(String name);
 }
