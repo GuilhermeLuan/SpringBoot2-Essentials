6ff2e51e03570dd5cc989e48a785664a10a04536
Guilherme Luan <guilhermeluan@pm.me>
2024-07-08 20:27:23 -0300

    Spring Data JPA Test

--- src/main/java/academey/devdojo/springboot2_essentials/domain/Anime.java
+++ src/main/java/academey/devdojo/springboot2_essentials/domain/Anime.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,6 +20,7 @@ public class Anime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @NotEmpty(message = "The anime name cannot be empty")
+    @NotBlank(message = "The anime name cannot be blank")
     private String name;
 }
